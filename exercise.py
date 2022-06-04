3074b8399527475458c6fc8003b759c317566065
wds33817 <91014157+wds33817@users.noreply.github.com>
2022-06-04 17:13:12 +1000

    Create exercise.py

---
+++ exercise.py
@@ -0,0 +1,31 @@
+#Create a tuple, contains the odd number from 1 to 20
+m = ()
+for i in range(1,21):
+    if (i % 2 != 0):
+        m+=(i,)
+print(m) 
+    
+#Create a list, repeat 1 to 5 sequentially 3 times
+m = []
+for i in range(1,6):
+    j = 3
+    while (j > 0):
+        m.append(i)
+        j-=1
+print(m)
+
+#Create a list, contains the multiples of 50
+m = []
+for i in range(1,50):
+    if (i % 5 == 0):
+        m.append(i)
+print(m)
+
+#Create a set e, contains "NASDAQ", "Dowjones", "DAX", "FTSE"
+e = ("NASDAQ", "Dowjones", "DAX", "FTSE")
+print(e)
+
+#Transform  "abc" to tuple, list, set:
+a = ("abc",)
+print(type(a))
+
