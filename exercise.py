198f05439d6aeef0926b853026b5b24de23a44bb
wds33817 <91014157+wds33817@users.noreply.github.com>
2022-12-18 21:03:38 +1100

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -111,3 +111,83 @@ else:
 
 
 
+
+    
+    
+    
+    #Write a function called to_metric. to_metric should take
+#as input one parameter, a string. The string will represent
+#a quantity in imperial volume units, such as "7 cups", "2
+#tablespoons", or "8 gallons". to_metric should return the
+#equivalent number of milliliters as a float. Round the
+#result to two decimal places.
+#
+#The possible imperial units to handle and their conversion to
+#milliliters are:
+#
+# - gallons: 3785.41 milliliters
+# - quarts: 946.35 milliliters
+# - pints: 473.18 milliliters
+# - cups: 240 milliliters
+# - ounces: 29.57 milliliters
+# - tablespoons: 14.79 milliliters
+# - teaspoons: 4.93 milliliters
+#
+#Return only the float representing the number of milliliters,
+#not the label. For example:
+#
+#to_metric("7.0 cups") -> 1680
+#to_metric("2.0 tablespoons") -> 29.58
+#to_metric("8.0 gallons") -> 30283.28
+#
+#You may assume that the string will be formatted like the
+#strings above: a decimal number, then a space, then one of
+#the following words: cgallons, quarts, pints, cups, ounces,
+#tablespoons, teaspoons
+
+
+#Add your code here!
+
+def to_metric(imperial_volume):
+# Split the string into value and unit
+    value, unit = imperial_volume.split()
+
+    # Convert the value to a float
+    value = float(value)
+
+    # Determine the conversion factor based on the unit
+    if unit == "gallons":
+        factor = 3785.41
+    elif unit == "quarts":
+        factor = 946.35
+    elif unit == "pints":
+        factor = 473.18
+    elif unit == "cups":
+        factor = 240
+    elif unit == "ounces":
+        factor = 29.57
+    elif unit == "tablespoons":
+        factor = 14.79
+    elif unit == "teaspoons":
+        factor = 4.93
+
+    # Convert the value to milliliters and return it
+    return round(value * factor, 2)
+
+#Below are some lines of code that will test your function.
+#You can change the value of the variable(s) to test your
+#function with different inputs.
+#
+#If your function works correctly, this will originally
+#print:
+#1680
+#29.58
+#30283.28
+#print(to_metric("7.0 cups"))
+#print(to_metric("2.0 tablespoons"))
+#print(to_metric("8.0 gallons"))
+#
+
+print(to_metric("7.0 cups"))
+print(to_metric("2.0 tablespoons"))
+print(to_metric("8.0 gallons"))
